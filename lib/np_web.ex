c029860b8bea4752af61a1ec71dd272984aea643
Kleidukos <uniaika@crypto-keupone.eu>
2018-08-11 22:53:40 +0200

    Allow subfolder templates

--- lib/np_web.ex
+++ lib/np_web.ex
@@ -30,7 +30,8 @@ defmodule NpWeb do
   def view do
     quote do
       use Phoenix.View, root: "lib/np_web/templates",
-                        namespace: NpWeb
+                        namespace: NpWeb,
+                        pattern: "**/*"
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
