4da389fa4e4cf9e45cdb72ab342e7951bb8d9b21
Kleidukos <uniaika@crypto-keupone.eu>
2018-09-03 19:05:07 +0200

    Add `has_links?` helper to AlbumController

--- lib/np_web/views/album_view.ex
+++ lib/np_web/views/album_view.ex
@@ -1,3 +1,12 @@
 defmodule NpWeb.AlbumView do
   use NpWeb, :view
+  alias Np.Resources.Album
+
+  def has_links?(%Album{}=album) do
+    al.links
+    |> Map.from_struct
+    |> Map.delete(:id)
+    |> Map.values
+    |> Enum.all(&!is_nil(&1))
+  end
 end
