877d1f3dc9ad6c39176ea5778b2c2cc9c6bec46d
Glitch Family <>
2018-09-09 19:21:58 +0200

    Add new catalog page's design

--- lib/np_web/controllers/page_controller.ex
+++ lib/np_web/controllers/page_controller.ex
@@ -4,10 +4,10 @@ defmodule NpWeb.PageController do
   def page(conn, %{"number" => offset}) do
     page = Np.Resources.list_albums(limit: 6, offset: offset)
     case offset do
-      "1" -> 
+      "1" ->
         render conn, "home.html", albums: page.entries, page: page
       _ ->
-        render conn "index.html", albums: page.entries, page: page
+        render conn, "catalog.html", albums: page.entries, page: page
     end
   end
 
