c28d7993c3e8a7a16cb9656cb88e64fc7363dec7
Kleidukos <kleidukos@crypto-keupone.eu>
2018-12-16 16:20:33 +0100

    feat(front): Enable streaming service link pre-fill on edit

--- lib/np/resources/album.ex
+++ lib/np/resources/album.ex
@@ -12,7 +12,7 @@ defmodule Np.Resources.Album do
     field :name, :string
     field :hash, :string
     field :slug, :string
-    
+
     embeds_one :links, Links, on_replace: :delete
 
     many_to_many :tags, Tag,
@@ -35,23 +35,23 @@ defmodule Np.Resources.Album do
   def update_changeset(%__MODULE__{}=album, attrs \\ %{}) do
     Logger.info "Updating #{attrs.name}"
     tags = parse_tags(attrs.tags)
-    links = attrs.links 
+    links = attrs.links
 
     album
     |> changeset(attrs)
     |> put_change(:links, links)
-    |> put_assoc(:tags, tags) 
+    |> put_assoc(:tags, tags)
   end
 
   def register_changeset(%__MODULE__{}=album, attrs \\ %{}) do
     Logger.info "Registering #{attrs.name}"
     tags = parse_tags(attrs.tags)
-    links = attrs.links 
+    links = attrs.links
 
     album
     |> changeset(attrs)
     |> put_change(:links, links)
-    |> put_assoc(:tags, tags) 
+    |> put_assoc(:tags, tags)
     |> put_slug
   end
 
@@ -77,4 +77,39 @@ defmodule Np.Resources.Album do
 
 
   def get_or_insert_tag(name), do: Repo.get_by(Tag, name: name) || Repo.insert!(%Tag{name: name})
+
+end
+
+defimpl Enumerable, for: Np.Resources.Album.Links do
+
+	def sanitize(map) do
+    map
+    |> Map.delete(:id)
+    |> Map.delete(:__struct__)
+	end
+
+  def count(map) do
+    {:ok, map |> sanitize |> map_size}
+  end
+
+  def member?(map, {key, value}) do
+    {:ok, match?(%{^key => ^value}, sanitize(map))}
+  end
+
+  def member?(_map, _other) do
+    {:ok, false}
+  end
+
+  def slice(map) do
+    {:ok, map_size(sanitize(map)), &Enumerable.List.slice(:maps.to_list(sanitize(map)), &1, &2)}
+  end
+
+  def reduce(map, acc, fun) do
+	  reduce_list(:maps.to_list(sanitize(map)), acc, fun)
+	end
+
+  defp reduce_list(_list, {:halt, acc}, _fun), do: {:halted, acc}
+  defp reduce_list(list, {:suspend, acc}, fun), do: {:suspended, acc, &reduce_list(list, &1, fun)}
+  defp reduce_list([], {:cont, acc}, _fun), do: {:done, acc}
+  defp reduce_list([head | tail], {:cont, acc}, fun), do: reduce_list(tail, fun.(head, acc), fun)
 end
