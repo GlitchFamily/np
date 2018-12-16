2031bba660b17f1ee60a798263871fbd259967ba
Kleidukos <kleidukos@crypto-keupone.eu>
2018-12-16 16:25:46 +0100

    chore(seeds): default username

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,4 +5,4 @@ results = YamlElixir.read_from_file!("priv/repo/seeds.yml")
 
 Enum.each results, fn album -> Np.Resources.create_album(album) end
 
-Accounts.create_user(%{username: "tekkoninkreet", password: "concrete", email: "i_love@ani.me"})
+Accounts.create_user(%{username: "tekkonkinkreet", password: "concrete", email: "i_love@ani.me"})
