16aea6600d178eac5522f9ccdfd226d1beb3d94f
Kleidukos <kleidukos@crypto-keupone.eu>
2018-12-18 12:01:47 +0100

    héhéhé.

--- lib/np/config.ex
+++ lib/np/config.ex
@@ -8,11 +8,13 @@ defmodule Np.Config do
     secret_key_base = get_env("SECRET_KEY_BASE")
     database_url    = get_env("DATABASE_URL")
 
-    Application.put_env(:np, NpWeb.Endpoint, [
-      http: [:inet6, port: port || 4000],
-      url: [host: hostname, port: 80],
-      secret_key_base: secret_key_base
-      ])
+    conf = [endpoint: Application.get_env(:np, NpWeb.Endpoint)]
+    new_conf = [endpoint: [http: [:inet6, port: port || 4000],
+                           url: [host: hostname, port: 80],
+                           secret_key_base: secret_key_base
+                          ]]
+
+    Application.put_env(:np, NpWeb.Endpoint, merge(conf, new_conf) |> Keyword.get(:endpoint))
 
     Application.put_env(:np, Np.Repo, [
       url: database_url,
@@ -26,6 +28,20 @@ defmodule Np.Config do
         raise(ArgumentError, "Variable #{var} absent from environment")
         System.stop(1)
       value -> value
+    end
+  end
+
+  def merge(config1, config2) do
+    Keyword.merge(config1, config2, fn _, app1, app2 ->
+      Keyword.merge(app1, app2, &deep_merge/3)
+    end)
+  end
+
+  defp deep_merge(_key, value1, value2) do
+    if Keyword.keyword?(value1) and Keyword.keyword?(value2) do
+      Keyword.merge(value1, value2, &deep_merge/3)
+    else
+      value2
     end
   end
 end
